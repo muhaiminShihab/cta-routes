4029b961904d49b47eb4995217aa0b93c0a809e0
Muhaimin Shihab <talk.shihab@gmail.com>
2024-11-03 12:50:56 +0600

    update: send flash message to user to get notification

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('cta-routes')->group(function () {
     Route::get('/storage-link', function () {
         Artisan::call('storage:link');
-        return back();
+        return back()->with('message', 'Storage link created successfully.');
     });
-    
+
     Route::get('/optimize-clear', function () {
         Artisan::call('optimize:clear');
-        return back();
+        return back()->with('message', 'Application optimized and cache cleared.');
     });
-    
+
     Route::get('/cache-clear', function () {
         Artisan::call('cache:clear');
-        return back();
+        return back()->with('message', 'Application cache cleared.');
     });
-    
+
     Route::get('/config-cache', function () {
         Artisan::call('config:cache');
-        return back();
+        return back()->with('message', 'Configuration cache created successfully.');
     });
-    
+
     Route::get('/migrate', function () {
         Artisan::call('migrate');
-        return back();
+        return back()->with('message', 'Database migration completed successfully.');
     });
-    
+
     Route::get('/migrate-fresh', function () {
         Artisan::call('migrate:fresh');
-        return back();
+        return back()->with('message', 'Database migration refreshed successfully.');
     });
-    
+
     Route::get('/migrate-seed', function () {
         Artisan::call('db:seed');
-        return back();
+        return back()->with('message', 'Database seeded successfully.');
     });
 });
